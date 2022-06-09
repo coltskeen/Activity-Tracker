99a092ea21f279470dc324b369b8bf15aca19ad5
Colt Skeen <colton.skeen@gmail.com>
2022-06-09 11:55:18 -0400

    built intialize layout

---
+++ Activity-Tracker/Pages/_ProgressCirclePartial.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Activity_Tracker.Pages
+{
+	public class ProgressCirclePartialModel : PageModel
+    {
+        public string BackgroundStroke { get; set; }
+        public string ForegroundStroke { get; set; }
+        public double PercentProgress { get; set; }
+        public double DisplayNumber { get; set; }
+        public string Unit { get; set; }
+        public string IconUrl { get; set; }
+    }
+}
